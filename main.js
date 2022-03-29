ad6c1c575aa71ffeef035b2ddbaf8cc3cc3db92c
Elivan-Jr <elivanlimajunior@gmail.com>
2022-03-29 19:08:47 -0300

    O que vimos até agora.
    Play no JS:
    
    Como manipular a tag <audio> do HTML através do JavaScript, como selecionar um elemento a partir de um seletor de id e a reproduzir um som a partir da função play(). Além disso, viu também como os erros são apresentados na aba Console da ferramenta DevTools.
    
    O que é uma função?:
    
    O que é uma função, para que servem, como declará-la e sua sintaxe básica. Além disso, viu que a ordem de execução de um código JavaScript importa, desde a inserção da tag script antes do fechamos da tag </body> no HTML até a ordem do código JavaScript dentro do seu próprio arquivo.
    
    Clique no botão:
    
    A utilizar o onclick como atributo no JavaScript, e como atribuir uma função ao onclcik sem que esta função seja invocada imediatamente.

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 function tocaSomPom () {
     document.querySelector('#som_tecla_pom').play();
 }
-document.querySelector('.tecla_pom');
+
+/*document: Acessar o documento.
+querySelector(''): Selecionar tag ou classe dentro do nosso HTML
+.onclick = FUNÇÃO(): ativar função quando clicar*/
+document.querySelector('.tecla_pom').onclick = tocaSomPom; /*Se colocarmos as aspas no final dessa função, ela será execultada assim que o navegador for recarregado. Então para que isso não aconteça deixamos sem as aspas.*/
