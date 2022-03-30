87e7b85b153f538275b605f44d6fca2fb177d803
Elivan-Jr <elivanlimajunior@gmail.com>
2022-03-30 19:33:38 -0300

    Sons nas teclas
    Colocando sons em todas as teclas.

--- main.js
+++ main.js
@@ -6,3 +6,51 @@ function tocaSomPom () {
 querySelector(''): Selecionar tag ou classe dentro do nosso HTML
 .onclick = FUNÇÃO(): ativar função quando clicar*/
 document.querySelector('.tecla_pom').onclick = tocaSomPom; /*Se colocarmos as aspas no final dessa função, ela será execultada assim que o navegador for recarregado. Então para que isso não aconteça deixamos sem as aspas.*/
+
+function tocaSomClap () {
+    document.querySelector('#som_tecla_clap').play();
+}
+
+document.querySelector('.tecla_clap').onclick = tocaSomClap;
+
+function tocaSomTim () {
+    document.querySelector('#som_tecla_tim').play();
+}
+
+document.querySelector('.tecla_tim').onclick = tocaSomTim;
+
+function tocaSomPuff () {
+    document.querySelector('#som_tecla_puff').play();
+}
+
+document.querySelector('.tecla_puff').onclick = tocaSomPuff;
+
+function tocaSomSplash () {
+    document.querySelector('#som_tecla_splash').play();
+}
+
+document.querySelector('.tecla_splash').onclick = tocaSomSplash;
+
+function tocaSomToim () {
+    document.querySelector('#som_tecla_toim').play();
+}
+
+document.querySelector('.tecla_toim').onclick = tocaSomToim;
+
+function tocaSomPsh () {
+    document.querySelector('#som_tecla_psh').play();
+}
+
+document.querySelector('.tecla_psh').onclick = tocaSomPsh;
+
+function tocaSomTic () {
+    document.querySelector('#som_tecla_tic').play();
+}
+
+document.querySelector('.tecla_tic').onclick = tocaSomTic;
+
+function tocaSomTom () {
+    document.querySelector('#som_tecla_tom').play();
+}
+
+document.querySelector('.tecla_tom').onclick = tocaSomTom;
