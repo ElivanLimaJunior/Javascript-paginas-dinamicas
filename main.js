6d1fc4378c90dcd013a4fde386cc7548f9f66b94
Elivan-Jr <elivanlimajunior@gmail.com>
2022-03-30 19:56:55 -0300

    O que eu vi até agora
    Lista de elementos:
    O que são os comentários de código e como utilizá-los para auxiliar durante a escrita da nossa aplicação. Vimos também uma alternativa ao querySelector quando necessário buscar muitos elementos, que é o querySelectorAll, que retorna uma lista (NodeList) com todos os elementos do seletor informado evitando a repetição de código.
    
    Referências:
    Para que serve uma referência e como declaramos uma referência de valor constante no código.
    
    Conhecendo listas:
    A estrutura de uma lista em JavaScript e como podemos acessar os elementos a partir dos índices com a sintaxe do colchetes.

--- main.js
+++ main.js
@@ -2,55 +2,9 @@ function tocaSomPom () {
     document.querySelector('#som_tecla_pom').play();
 }
 
-/*document: Acessar o documento.
-querySelector(''): Selecionar tag ou classe dentro do nosso HTML
-.onclick = FUNÇÃO(): ativar função quando clicar*/
-document.querySelector('.tecla_pom').onclick = tocaSomPom; /*Se colocarmos as aspas no final dessa função, ela será execultada assim que o navegador for recarregado. Então para que isso não aconteça deixamos sem as aspas.*/
+//Primeiro criamos uma constante com o nome de listaDeTeclas, nela declaramos o document.querySelectorAll para que ele possa pegar todas as classes com o nome .tecla
+const listaDeTeclas = document.querySelectorAll('.tecla');
+//Essa declaração nos retorna uma lista com todas as teclas declaradas com essa classe. Então agora o listaDeTeclas passa a ser uma lista com as 9 teclas armazenadas.
 
-function tocaSomClap () {
-    document.querySelector('#som_tecla_clap').play();
-}
-
-document.querySelector('.tecla_clap').onclick = tocaSomClap;
-
-function tocaSomTim () {
-    document.querySelector('#som_tecla_tim').play();
-}
-
-document.querySelector('.tecla_tim').onclick = tocaSomTim;
-
-function tocaSomPuff () {
-    document.querySelector('#som_tecla_puff').play();
-}
-
-document.querySelector('.tecla_puff').onclick = tocaSomPuff;
-
-function tocaSomSplash () {
-    document.querySelector('#som_tecla_splash').play();
-}
-
-document.querySelector('.tecla_splash').onclick = tocaSomSplash;
-
-function tocaSomToim () {
-    document.querySelector('#som_tecla_toim').play();
-}
-
-document.querySelector('.tecla_toim').onclick = tocaSomToim;
-
-function tocaSomPsh () {
-    document.querySelector('#som_tecla_psh').play();
-}
-
-document.querySelector('.tecla_psh').onclick = tocaSomPsh;
-
-function tocaSomTic () {
-    document.querySelector('#som_tecla_tic').play();
-}
-
-document.querySelector('.tecla_tic').onclick = tocaSomTic;
-
-function tocaSomTom () {
-    document.querySelector('#som_tecla_tom').play();
-}
-
-document.querySelector('.tecla_tom').onclick = tocaSomTom;
+//pegamos a nossa lista e escolhemos a tecla 1 que é representado como 0 na programação. Então colocamos nela o onclick e a função tocaSomPom.
+listaDeTeclas[0].onclick = tocaSomPom;
