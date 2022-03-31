b6eee1f2acf3a67b7c86e537209e0905533639a2
Elivan-Jr <elivanlimajunior@gmail.com>
2022-03-31 19:17:30 -0300

    criando função

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
-function tocaSomPom () {
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom (idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
 }
 
-//Primeiro criamos uma constante com o nome de listaDeTeclas, nela declaramos o document.querySelectorAll para que ele possa pegar todas as classes com o nome .tecla
-const listaDeTeclas = document.querySelectorAll('.tecla');
-//Essa declaração nos retorna uma lista com todas as teclas declaradas com essa classe. Então agora o listaDeTeclas passa a ser uma lista com as 9 teclas armazenadas.
 
-//pegamos a nossa lista e escolhemos a tecla 1 que é representado como 0 na programação. Então colocamos nela o onclick e a função tocaSomPom.
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
 
 let contador = 0;
 //listaDeTeclas.length saber o tamanho exato da nossa lista
 while (contador < listaDeTeclas.length) {
 
-    listaDeTeclas[contador].onclick = tocaSomPom;
+    listaDeTeclas[contador].onclick = function () {
+        tocaSom('#som_tecla_pom')
+    };
 
     contador = contador + 1
 
