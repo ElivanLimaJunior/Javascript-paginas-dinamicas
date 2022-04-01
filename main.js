a71d514cd13fc52c0bf132626af8acae48c43102
Elivan-Jr <elivanlimajunior@gmail.com>
2022-04-01 19:19:06 -0300

    Comentando o código
    Apenas para teor de fixação.

--- main.js
+++ main.js
@@ -8,6 +8,7 @@ const listaDeTeclas = document.querySelectorAll('.tecla');
 
 let contador = 0;
 //listaDeTeclas.length saber o tamanho exato da nossa lista
+
 while (contador < listaDeTeclas.length) {
 
     listaDeTeclas[contador].onclick = function () {
@@ -18,3 +19,22 @@ while (contador < listaDeTeclas.length) {
 
     console.log(contador);
 }
+
+/* Primeiro nós criamos uma função "tocaSom "
+Dentro das () descrevemos o que vamos pegar de dentro do nosso código HTML, apenas escrevemos o que é para identificar.
+Após isso, abrimos as chaves {} e colocamos dentro a nossa função que é :
+
+        document.querySelector(idElementoAudio).play();
+
+        document : acessar HTML
+        querySelector() : Seleciona o que vamos selecionar
+        .play() : o que vai fazer ?
+
+
+após isso criamos uma const que recebeu listaDeTeclas.
+Então usamos o querySelectorAll('.tecla') para pegar todas as tags q tem a classe ".tecla" no documento fazendo assim uma lista.
+
+criamos um contador com o let que é um valor variavel, no nosso caso ele vai ser um contador.
+
+
+Após usamos o While para dar uma condição para nossa função ser ativada.
