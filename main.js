071611e987d1118a6476351a33b1359bf0ba5624
Elivan-Jr <elivanlimajunior@gmail.com>
2022-04-01 19:50:15 -0300

    Código finalizando e funcioando
    Agora nosso código está funcionando da maneira correta.

--- main.js
+++ main.js
@@ -11,27 +11,32 @@ let contador = 0;
 
 while (contador < listaDeTeclas.length) {
 
-    
-    const instrumento = listaDeTeclas[contador].classList[1];
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
 
-    console.log(instrumento)
+    console.log(instrumento);
+    //Template string abaixo
+    //Irá receber a string #som_+a string que está vindo dentro do indice de listaDeTeclas.
+    const idaudio = `#som_${instrumento}`
 
-    listaDeTeclas[contador].onclick = function () {
-        tocaSom('#som_tecla_pom')
-    };
+    tecla.onclick = function () {
+        tocaSom(idaudio);
+    }
 
-    contador = contador + 1
+    contador = contador + 1;
 
     console.log(contador);
 }
 
 /* no navegador, acessamos o console e digitamos:
 
- listaDeTeclas[0].classlist[]
+ listaDeTeclas[0].classlist[1]
+
+ Se observar no navegador, verá que o "classlist[1]" está acessando a segunda classe do nosso indice. Por isso da certo!!
 
  ela nos retorna uma lista de classes, então podemos numerar a .classlist[0] por ex: para acessar a primeira classe da lista.
  
  Class.list:
- 
+
  classList é uma propriedade somente leitura que retorna uma coleção DOMTokenList (en-US) ativa dos atributos de classe do elemento. Usar classList é uma alternativa conveniente para acessar a lista de classes de um elemento como uma seqüência delimitada por espaço através de element. className
  */
